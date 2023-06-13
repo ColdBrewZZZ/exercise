1e067361f06bda3dc8ac90295a20f3e386bea8e8
zoe <z.g.bradford@gmail.com>
2023-06-13 08:08:24 -0500

    new project

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import  from "./";
+
+class App extends Component {
+    state = {  } 
+    render() { 
+        return (</> );
+    }
+}
+ 
+export default App;
